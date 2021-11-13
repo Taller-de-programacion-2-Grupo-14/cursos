3478290d4fd68cfb8de6ffef54fa8015c095686a
violeta <viperez@fi.uba.ar>
2021-11-12 23:45:44 -0300

    delete unnecesary files

--- db/database.py
+++
@@ -1,7 +0,0 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine(os.environ.get("DATABASE_URL"), echo=True, future=True)
-
-Base = declarative_base()
