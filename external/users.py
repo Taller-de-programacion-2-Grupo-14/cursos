54262b2256bf012239c142dd7bffa5a1161b2ae4
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-11-15 22:47:52 -0300

    fix linter

--- external/users.py
+++ external/users.py
@@ -5,7 +5,7 @@ import requests
 
 class Users:
     def __init__(self):
-        self.host = os.environ.get('USERS_HOSTNAME')
+        self.host = os.environ.get("USERS_HOSTNAME")
 
     def getUser(self, userId: int):
         response = requests.get(f"{self.host}users?id={userId}")
